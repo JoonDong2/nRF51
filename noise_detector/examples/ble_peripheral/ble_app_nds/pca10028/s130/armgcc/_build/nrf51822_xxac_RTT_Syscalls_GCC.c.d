8982cb359ba230d7db9ac4fb8b879a5f59ad25d0
JoonDong2 <kwonjoondong@gmail.com>
2018-02-01 03:01:01 +0900

    delete _build

--- noise_detector/examples/ble_peripheral/ble_app_nds/pca10028/s130/armgcc/_build/nrf51822_xxac_RTT_Syscalls_GCC.c.d
+++
@@ -1,2 +0,0 @@
-_build/nrf51822_xxac_RTT_Syscalls_GCC.c.o: \
- ../../../../../../external/segger_rtt/RTT_Syscalls_GCC.c
--- noise_detector/examples/ble_peripheral/ble_app_nds/pca10028/s130/armgcc/_build/nrf51822_xxac_SEGGER_RTT_printf.c.d
+++
@@ -1,2 +0,0 @@
-_build/nrf51822_xxac_SEGGER_RTT_printf.c.o: \
- ../../../../../../external/segger_rtt/SEGGER_RTT_printf.c
